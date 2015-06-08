58dab3f0306d14f0d044a691760539512a93c3c9
MarcelSato <marcelfsato@gmail.com>
2015-06-08 14:27:44 +0100

    New Exercise added

---
+++ src/Test/TestObj.java
@@ -0,0 +1,20 @@
+package Test;
+
+/**
+ * Created by Marcel on 08/06/15.
+ */
+public class TestObj
+{
+    public static void main (String [] args)
+    {
+        Object o = new Object() /* Line 5 */
+        {
+            public boolean equals(Object obj)
+            {
+                return true;
+            }
+        };      /* Line 11 */
+
+        System.out.println(o.equals("Fred"));
+    }
+}
