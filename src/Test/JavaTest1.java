51cdef084ec4da4d810f2b5a7dc730e7b734bab5
MarcelSato <marcelfsato@gmail.com>
2015-06-12 14:40:14 +0100

    Added new exercise

--- src/Test/JavaTest1.java
+++ src/Test/JavaTest1.java
@@ -2,7 +2,6 @@ package Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
---
+++ src/Test/JavaTest2.java
@@ -0,0 +1,10 @@
+package Test;
+
+/**
+ * Created by Marcel on 12/06/15.
+ */
+public class JavaTest2 {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(9));
+    }
+}
